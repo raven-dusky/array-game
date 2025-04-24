1c7e03dc0f0887b57b9fc3b9255e3e95042c539c
Giulio Bitetto <raven.dusky@icloud.com>
2025-04-24 19:08:04 +0200

    Finalize upgrade logic; upgrades now affect the generate() function

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Score;
+use App\Models\Upgrade;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,8 +11,14 @@ use Illuminate\Support\Facades\Auth;
 class Counter extends Component
 {
     public function generate() {
+        $upgrade = Upgrade::where('user_id', Auth::id())->first();
+        $multiplier = 1 
+        + ($upgrade->loop_enhancer ?? 0) * 0.1
+        + ($upgrade->stack_overdrive ?? 0) * 0.1
+        + ($upgrade->bit_splitter ?? 0) * 0.3
+        + ($upgrade->garbage_collector ?? 0) * 0.4;
         $counter = Score::where('user_id', Auth::id())->first();
-        $counter->total++;
+        $counter->total += $multiplier;
         $counter->save();
     }
 
